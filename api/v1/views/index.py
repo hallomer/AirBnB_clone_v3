54ff5f3feff1aec3d69ca8a2f3e553cb07e7ef4c
Hiba <hebaaltayeb2@icloud.com>
2024-06-08 05:29:35 +0300

    checking

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 """Defines the index view."""
-from flask import json, Response
+from flask import jsonify
 from api.v1.views import app_views
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """Returns the status of the API"""
-    response_data = {
-        "status": "OK"
-    }
-    response_json = json.dumps(response_data, indent=2, sort_keys=True) + '\n'
-    return Response(response_json, content_type="application/json")
+    return jsonify({"status": "OK"})
