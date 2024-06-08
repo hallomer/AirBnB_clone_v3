2b55b99a305baa4e1eb5e3b9fb7d0158fdc10b31
hiba <hebaaltayeb2@icloud.com>
2024-06-08 02:05:40 +0000

    fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,7 @@ from api.v1.views import app_views
 @app_views.route('/status', strict_slashes=False)
 def status():
     """Returns the status of the API"""
-    status = {
-	    "status": "OK"
-    }    
-    return jsonify(status)
+    reponse = {
+        "status": "OK"
+    }
+    return jsonify(reponse)
