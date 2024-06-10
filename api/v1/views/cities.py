81abdaca97d94f121973e2698e54d187f3537e9c
Hiba <hebaaltayeb2@icloud.com>
2024-06-10 08:46:17 +0300

    hope2

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -17,8 +17,7 @@ def get_cities(state_id):
     return jsonify([city.to_dict() for city in state.cities])
 
 
-@app_views.route('/cities/<city_id>', methods=['GET'],
-                 strict_slashes=False)
+@app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
     """Retrieves a City object."""
     city = storage.get(City, city_id)
@@ -27,8 +26,7 @@ def get_city(city_id):
     return jsonify(city.to_dict())
 
 
-@app_views.route('/cities/<city_id>', methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
     """Deletes a City object."""
     city = storage.get(City, city_id)
@@ -39,18 +37,19 @@ def delete_city(city_id):
     return jsonify({}), 200
 
 
-@app_views.route('/states/<state_id>/cities',
-                 methods=['POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
 def create_city(state_id):
     """Creates a City."""
     state = storage.get(State, state_id)
     if not state:
         abort(404)
-    if not request.json:
-        abort(400, description="Not a JSON")
-    if 'name' not in request.json:
-        abort(400, description="Missing name")
-    new_city = City(state_id=state_id, **request.get_json())
+    if not request.is_json:
+        abort(400, 'Not a JSON')
+    data = request.get_json()
+    if 'name' not in data:
+        abort(400, 'Missing name')
+    new_city = City(state_id=state_id, **data)
     new_city.save()
     return jsonify(new_city.to_dict()), 201
 
@@ -61,9 +60,10 @@ def update_city(city_id):
     city = storage.get(City, city_id)
     if not city:
         abort(404)
-    if not request.json:
-        abort(400, description="Not a JSON")
-    for key, value in request.get_json().items():
+    if not request.is_json:
+        abort(400, 'Not a JSON')
+    data = request.get_json()
+    for key, value in data.items():
         if key not in ['id', 'state_id', 'created_at', 'updated_at']:
             setattr(city, key, value)
     city.save()
