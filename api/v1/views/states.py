fc4a4ba59e5f20953b99f11c049c537c863abd8a
Hiba <hebaaltayeb2@icloud.com>
2024-06-10 07:34:00 +0300

    try

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Handles all default RESTFul API actions."""
-from flask import jsonify, abort, request, make_response
+from flask import jsonify, abort, request
 from api.v1.views import app_views
 from models import storage
 from models.state import State
@@ -17,7 +17,7 @@ def get_states():
 def get_state(state_id):
     """Retrieves a State object."""
     state = storage.get(State, state_id)
-    if state is None:
+    if not state:
         abort(404)
     return jsonify(state.to_dict())
 
@@ -27,11 +27,11 @@ def get_state(state_id):
 def delete_state(state_id):
     """Deletes a State object."""
     state = storage.get(State, state_id)
-    if state is None:
+    if not state:
         abort(404)
     storage.delete(state)
     storage.save()
-    return make_response(jsonify({}), 200)
+    return jsonify({}), 200
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
@@ -43,14 +43,14 @@ def create_state():
         abort(400, description="Missing name")
     new_state = State(**request.get_json())
     new_state.save()
-    return make_response(jsonify(new_state.to_dict()), 201)
+    return jsonify(new_state.to_dict()), 201
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
     """Updates a State object."""
     state = storage.get(State, state_id)
-    if state is None:
+    if not state:
         abort(404)
     if not request.json:
         abort(400, description="Not a JSON")
@@ -58,4 +58,4 @@ def update_state(state_id):
         if key not in ['id', 'created_at', 'updated_at']:
             setattr(state, key, value)
     state.save()
-    return make_response(jsonify(state.to_dict()), 200)
+    return jsonify(state.to_dict()), 200
