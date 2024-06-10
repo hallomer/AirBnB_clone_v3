bda2e62527c199aad713fdf8c08a3848f5cdc71a
Hiba <hebaaltayeb2@icloud.com>
2024-06-10 06:59:14 +0300

    fixes

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -19,7 +19,7 @@ def get_state(state_id):
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
-    return jsonify(state.to_dict)
+    return jsonify(state.to_dict())
 
 
 @app_views.route('/states/<state_id>',
