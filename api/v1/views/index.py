2064fdf1e2e7e7de8a88da15bf59e7f8678eb956
Hiba <hebaaltayeb2@icloud.com>
2024-06-10 04:46:46 +0300

    some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines the index view."""
 from flask import jsonify
+from models import storage
 from api.v1.views import app_views
 
 
@@ -8,3 +9,16 @@ from api.v1.views import app_views
 def status():
     """Returns the status of the API"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def stats():
+    """Retrieves the number of each objects by type"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+    return jsonify(stats)
